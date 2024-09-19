be73f5e06fadf92f86ee043d4826786e403ba284
Allen Liu <85519235+LSJ-Allen@users.noreply.github.com>
2024-09-18 23:51:22 -0400

    added education page

---
+++ app/educations/page.js
@@ -0,0 +1,13 @@
+import EducationJourney from "@/components/EducationJourney/EducationJourney";
+import NavBar from "@/components/NavBar";
+
+const Educations = () => {
+    return ( 
+        <div>
+            <NavBar/>
+            <EducationJourney/>
+        </div>
+    );
+}
+ 
+export default Educations;
