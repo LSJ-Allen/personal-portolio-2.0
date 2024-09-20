db4437c08de7f6f2956f64bdbc6fbe41f689e21c
Allen Liu <85519235+LSJ-Allen@users.noreply.github.com>
2024-09-20 00:35:21 -0400

    added width limitation to home page content

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -10,10 +10,18 @@ import ContactSection from './ContactSection/ContactSection';
 const Layout = ({ children }) => (
   <div className="flex overflow-hidden flex-col text-base bg-black text-neutral-400">
     <NavBar />
-    <main>{children}</main>
-    <PersonalJourney/>
-    <AboutMe/>
-    <ContactSection/>
+    <div className='flex justify-center items-center bg-zinc-800'>
+      <div className='max-w-[1300px] bg-zinc-800'>
+        <main>{children}</main>
+
+        <PersonalJourney/>
+        <AboutMe/>
+        <ContactSection/>
+      </div>
+    </div>
+
+
+    
   </div>
 );
 
