f9462c9860529875ee4cdc24003faf64aa764c56
Allen Liu <85519235+LSJ-Allen@users.noreply.github.com>
2024-10-01 22:52:36 -0400

    fixed image paths

--- components/ContactInfo.jsx
+++ components/ContactInfo.jsx
@@ -22,16 +22,16 @@ const ContactInfo = () => (
   <section className="w-full py-8 font-semibold">
     <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
       <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
-        <ContactItem src="/images/email-icon.png" alt="Email Icon">
+        <ContactItem src="./images/email-icon.png" alt="Email Icon">
           lsj.liu@mail.utoronto.ca
         </ContactItem>
         <Link href="https://www.linkedin.com/in/shijia-liu123/" target="_blank" rel="noopener noreferrer" className="flex justify-center">
-          <ContactItem src="/images/linkedin-icon.png" alt="LinkedIn icon">
+          <ContactItem src="./images/linkedin-icon.png" alt="LinkedIn icon">
             LinkedIn
           </ContactItem>
         </Link>
         <Link href="https://github.com/LSJ-Allen" target="_blank" rel="noopener noreferrer" className="flex justify-center">
-          <ContactItem src="/images/github-icon.png" alt="GitHub icon">
+          <ContactItem src="./images/github-icon.png" alt="GitHub icon">
             GitHub
           </ContactItem>
         </Link>
